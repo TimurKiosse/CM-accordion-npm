4786b26c62447214128879e5a4350a0747b1ab5f
timur <timur@addatech.ca>
2023-05-30 15:04:52 -0400

    Local library

---
+++ projects/adda-accordion/src/lib/adda-accordion.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-adda-accordion',
+  template: `
+    <p>
+      adda-accordion works!
+      test WATCH
+    </p>
+    <p>WORKS FINE!!!</p>
+
+    <p>AGAIN</p>
+
+    <p>AND AGAIN</p>
+  `,
+  styles: [
+  ]
+})
+export class AddaAccordionComponent {
+
+}
---
+++ projects/adda-accordion/src/lib/adda-accordion.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { AddaAccordionComponent } from './adda-accordion.component';
+
+
+
+@NgModule({
+  declarations: [
+    AddaAccordionComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    AddaAccordionComponent
+  ]
+})
+export class AddaAccordionModule { }
